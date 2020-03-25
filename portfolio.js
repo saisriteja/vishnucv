d2d7e0685fc91f8f6d36d2cae667573f4a284472
saisriteja <48018142+saisriteja@users.noreply.github.com>
2020-03-25 18:21:38 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,48 @@
+var e = document.getElementById('a');
+e.onmouseover = function() {
+  document.getElementById('Apopup').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('Apopup').style.display = 'none';
+}
+
+var e = document.getElementById('m');
+e.onmouseover = function() {
+  document.getElementById('Mpopup').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('Mpopup').style.display = 'none';
+}
+
+var e = document.getElementById('c');
+e.onmouseover = function() {
+  document.getElementById('Cpopup').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('Cpopup').style.display = 'none';
+}
+
+
+var e = document.getElementById('t');
+e.onmouseover = function() {
+  document.getElementById('Tpopup').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('Tpopup').style.display = 'none';
+}
+
+var e = document.getElementById('s');
+e.onmouseover = function() {
+  document.getElementById('Spopup').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('Spopup').style.display = 'none';
+}
+
+var e = document.getElementById('e');
+e.onmouseover = function() {
+  document.getElementById('Epopup').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('Epopup').style.display = 'none';
+}
